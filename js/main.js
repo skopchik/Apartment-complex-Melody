eaa9b2941e8e6e9619f9eef7998773c338d82114
skopchik <65411869+skopchik@users.noreply.github.com>
2021-08-21 19:02:08 +0300

    Интерактивное модальное окно для выбора квартир

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 $(document).ready(function () {
     var currentFloor = 2;
     var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
+
+    var flatPath = $(".flats path");
+    var flatLink = $(".flat-link");
     var counterUp = $(".counter-up");
     var counterDown = $(".counter-down");
+    var modal = $(".modal");
+    var modalCLoseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats-button");
 
+
+    flatPath.on("mouseover", function () {
+        flatLink.removeClass("flat-link-active");
+        flatPath.removeClass("current-flat");
+        var currentFlat = $(this).attr("data-flat");
+        $(`[data-flat=${currentFlat}]`).toggleClass("current-flat");
+        $(`[data-flat=${currentFlat}]`).toggleClass("flat-link-active");
+
+    });
+
+    flatLink.on("mouseover", function () {
+        flatPath.removeClass("current-flat");
+        flatLink.removeClass("flat-link-active");
+        var currentFlat = $(this).attr("data-flat");
+        $(`[data-flat=${currentFlat}]`).toggleClass("current-flat");
+        $(`[data-flat=${currentFlat}]`).toggleClass("flat-link-active");
+
+    });
 
     floorPath.on("mouseover", function () {
         floorPath.removeClass("current-floor");
@@ -11,6 +35,10 @@ $(document).ready(function () {
         $(".counter").text(currentFloor);
 
     });
+
+    floorPath.on("click", toggleModal);
+    modalCLoseButton.on("click", toggleModal);
+    viewFlatsButton.on("click", toggleModal);
 
     counterUp.on("click", function () {
         if (currentFloor < 18) {
@@ -33,4 +61,9 @@ $(document).ready(function () {
             $(`[data-floor=${stringCurrentFloor}]`).toggleClass("current-floor");
         }
     });
+
+
+    function toggleModal() {
+        modal.toggleClass("is-open");
+    }
 });
